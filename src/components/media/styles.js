8010311ddeb3b644ac99ebb77f6681c961d655df
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-03-15 23:21:55 -0300

    Update Media component container

--- src/components/media/styles.js
+++ src/components/media/styles.js
@@ -3,21 +3,23 @@ import styled from 'styled-components';
 export const Player = styled.iframe`
   width: 450px;
   height: 280px;
+  border: solid #44444433;
+  border-width: 1px;
+  margin-bottom: 1rem;
 `;
 
 export const PlayerContainer = styled.div`
   margin-left: auto;
 `;
 
-export const Title = styled.div`
+export const Title = styled.h2`
   font-size: 20px;
-  text-align: center;
-  padding-bottom: 1rem;
+  margin-top: 0px;
 `;
 
 export const Icon = styled.img`
   height: 40px;
   width: 40px;
-  margin: 4px;
+  margin-right: 15px;
   cursor: pointer;
 `;
