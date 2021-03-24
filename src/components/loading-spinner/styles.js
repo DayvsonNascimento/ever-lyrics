cdfdde17c1f1a17ff31f5e785e729c0fbe935192
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-03-23 22:20:21 -0300

    Add LoadingSpinner component

---
+++ src/components/loading-spinner/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 60vh;
+`;
+
+export const Spinner = styled.div`
+  display: inline-block;
+  width: 6rem;
+  height: 6rem;
+  vertical-align: text-bottom;
+  border: 0.25em solid;
+  border-right: 0.25em solid transparent;
+  border-radius: 50%;
+  animation: spinner 0.75s infinite linear;
+  color: #1a649c;
+
+  @keyframes spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
